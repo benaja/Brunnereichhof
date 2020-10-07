8b2e30f8b4d09ee387a753570199eb58ff99381c
benaja <benhu00@outlook.com>
2020-10-07 15:29:30 +0200

    add 5 hours

--- app/Employee.php
+++ app/Employee.php
@@ -104,7 +104,7 @@ class Employee extends Model
         if ($this->profileimage) {
             return Storage::disk('s3')->temporaryUrl(
                 $this->profileimage,
-                now()->addMinutes(5)
+                now()->addHours(5)
             );
         } else {
             return null;
