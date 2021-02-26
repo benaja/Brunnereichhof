d82c288787a616e732b9c7fe60bf5ab9c5386ff5
benaja <benhu00@outlook.com>
2021-02-26 16:21:30 +0100

    fix removing of employees

--- resources/frontend/src/components/ResourcePlanner/plan/CustomerCard.vue
+++ resources/frontend/src/components/ResourcePlanner/plan/CustomerCard.vue
@@ -108,7 +108,7 @@ export default {
     },
     removeEmployee(rapportdetailId) {
       this.axios.$delete(`customers/${this.customer.id}/rapportdetails/${rapportdetailId}`).then(() => {
-        const rapportdetail = this.customer.rapportdetails.find(r => r.id === rapportdetailId)
+        const rapportdetail = this.customer.rapportdetails.find(r => r.id === Number(rapportdetailId))
         const index = this.customer.rapportdetails.indexOf(rapportdetail)
         this.customer.rapportdetails.splice(index, 1)
       }).catch(() => {
