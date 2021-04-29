928402f1ef9ec0e61db593acb7fde8600e9a2a28
Benaja Hunzinger <benhu00@outlook.com>
2021-04-29 17:14:29 +0200

    add migrations for family allowances

---
+++ database/migrations/2021_04_26_145101_create_files_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('files', function (Blueprint $table) {
+            $table->id();
+            $table->string('type');
+            $table->string('path')->nullable();
+            $table->boolean('is_submitted')->nullable();
+            $table->date('expiration_date')->nullable();
+            $table->foreignId('filable_id')->nullable();
+            $table->string('filable_type')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('files');
+    }
+}
---
+++ database/migrations/2021_04_26_160828_create_family_allowance_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFamilyAllowanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('family_allowances', function (Blueprint $table) {
+            $table->id();
+            $table->string('civil_status')->nullable();
+            $table->boolean('has_faily_allowance')->default(false);
+            $table->date('expiration_of_family_allowance')->nullable();
+            $table->boolean('partner_employed')->nullable();
+            $table->boolean('needs_e411_form')->nullable();
+            $table->boolean('is_e411_handed_out')->nullable();
+            $table->boolean('is_e411_submitted')->nullable();
+            $table->boolean('has_marriage_document')->nullable();
+            $table->boolean('has_divorce_document')->nullable();
+            $table->boolean('it_registration_family_allowances_send')->nullable();
+            $table->date('family_allowance_expiration_date')->nullable();
+            $table->boolean('claim_id_received')->nullable();
+            $table->date('claim_id_expiration_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('family_allowances');
+    }
+}
