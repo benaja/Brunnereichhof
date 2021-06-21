161fa757e669fca17a5cf801f6bf92e1e35bfbf5
Benaja Hunzinger <benhu00@outlook.com>
2021-06-21 17:55:46 +0200

    create transactions for workers and employees

--- resources/frontend/src/views/UserRedirect.vue
+++ resources/frontend/src/views/UserRedirect.vue
@@ -12,7 +12,7 @@ export default {
       if (user.type_id === UserType.Employee) {
         this.$router.replace(`/employee/${user.employee.id}`)
       } else if (user.type_id === UserType.Customer) {
-        this.$router.replace(`/customer/${user.id}`)
+        this.$router.replace(`/customer/${user.customer.id}`)
       } else {
         this.$router.replace(`/worker/${user.id}`)
       }
